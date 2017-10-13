df13ef3f9d8b24825a841143884beeaf83df5c8b
Alex Lew <alexl.mail@gmail.com>
2017-10-13 09:41:38 -0400

    Add project files

---
+++ move/Move.asm
@@ -0,0 +1,14 @@
+// File name: Move.asm
+
+// Runs an infinite loop that listens to the keyboard input.
+// Draws a 16-by-16 square to the top left corner of the screen.
+// When a key is pressed and released (any key), the rectangle is
+// moved 16 pixels to the right.
+
+// Challenge versions (please specify if you are doing one of these):
+// (Only try these if they sound interesting to you.)
+//   - Instead of moving in 16-pixel increments, move in 5-pixel increments.
+//   - Instead of moving whenever a key is pressed and released,
+//       move gradually while a key is pressed down. <-- doing this one
+
+
---
+++ move/Move.tst
@@ -0,0 +1,8 @@
+// File name: Move.tst
+
+load Move.hack;
+echo "First, make sure that 'No Animation' is selected. Then, select the Keyboard, use the keyboard, and check the screen.";
+
+repeat {
+  ticktock;
+}
