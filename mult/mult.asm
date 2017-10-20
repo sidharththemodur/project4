1bed82a89e5fbb97ee59d8b0257a3f5ca7fd7c2c
Siddhartha Modur <publicinternetemail@gmail.com>
2017-10-20 10:58:59 -0400

    MULT
    Finished mult; passes tests

--- mult/mult.asm
+++ mult/mult.asm
@@ -6,4 +6,20 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+@R2
+M=0
+
+(loop)
+@R0
+D=M
+@end
+D;JEQ
+@R1
+D=M
+@R2
+M=M+D
+@R0
+M=M-1
+@loop
+0;JMP
+(end)
